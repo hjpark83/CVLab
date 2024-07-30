f0e29b2bcb24a2a95985e8fdf06979443eede59a
Hyunjoon <junippini83@hanyang.ac.kr>
2024-07-30 12:47:29 +0900

    CVLab

--- Paper Review/Segmentations/Implementation/main.py
+++ Paper Review/Segmentations/Implementation/main.py
@@ -9,7 +9,7 @@ from load_dataset import SegmentationDataset
 
 def main():
     # Get dataset directory from user input
-    base_dir = input("Enter the base directory for the dataset (e.g., 'dataset/flood-area-segmentation'): ")
+    base_dir = input("Enter the directory path for the dataset: ")
 
     # Define dataset directories based on user input
     csv_file = os.path.join(base_dir, 'metadata.csv')
@@ -29,14 +29,16 @@ def main():
     # Train the model
     model, train_losses, dataloader, device = train(dataloader=dataloader)
     
-    # Create output directory if it does not exist
-    if not os.path.exists('output'):
-        os.makedirs('output')
+    # Create output directories if they do not exist
+    output_dirs = ['output/original', 'output/ground_truth', 'output/prediction']
+    for dir in output_dirs:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
 
     # Visualize all results using OpenCV (save to disk)
     model.eval()
     with torch.no_grad():
-        img_counter = 0  # Counter to keep track of image index
+        img_num = 0  # Counter to keep track of image index
         for images, masks in dataloader:
             images = images.to(device)
             masks = masks.to(device).squeeze(1)
@@ -55,10 +57,10 @@ def main():
                 mask = (mask * 255).astype(np.uint8)
                 pred = (pred * 255).astype(np.uint8)
 
-                # Save images using OpenCV
-                cv2.imwrite(f'output/original_{img_counter}.png', img)
-                cv2.imwrite(f'output/ground_truth_{img_counter}.png', mask)
-                cv2.imwrite(f'output/prediction_{img_counter}.png', pred)
+                # Save images using OpenCV in separate directories
+                cv2.imwrite(f'output/original/original_{img_num}.png', img)
+                cv2.imwrite(f'output/ground_truth/ground_truth_{img_num}.png', mask)
+                cv2.imwrite(f'output/prediction/prediction_{img_num}.png', pred)
 
                 img_counter += 1  # Increment the image counter
 
